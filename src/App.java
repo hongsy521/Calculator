dc3235fb0f75ec679868f423f7719eb0b03811d8
hongsy521 <seoyeong0521@naver.com>
2024-04-26 23:19:17 +0900

    LEVEL1-(1)(2)(3)(4) commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-       // 계산기 프로젝트
+            // 계산기 프로젝트
+        Scanner sc = new Scanner(System.in);
+        int result=0;
+
+        while (true){
+            System.out.print("첫번째 숫자를 입력하세요 : ");
+            int num1 = sc.nextInt();
+            System.out.print("두번째 숫자를 입력하세요 : ");
+            int num2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요 : ");
+            char ch = sc.next().charAt(0);
+            switch (ch){
+                case '+':
+                    result=num1+num2;
+                    break;
+                case '-':
+                    result=num1-num2;
+                    break;
+                case '*':
+                    result=num1*num2;
+                    break;
+                case '/':
+                    if(num2 == 0){
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        continue;
+                    }else {
+                        result=num1/num2;
+                        break;
+                    }
+                default:
+                    System.out.println("잘못된 연산 기호입니다.");
+            }
+            System.out.println("결과 : "+result);
+            System.out.println("더 계산하시겠습니까? (exit 입력시 종료)");
+            String input=sc.next();
+            if(input.equals("exit")){
+                break;
+            } else {
+                continue;
+            }
+        }
+
         }
     }
