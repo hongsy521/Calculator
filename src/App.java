e51dfb5f7fa31eb27efa28fe456ad7fc1ae44ab2
hongsy521 <seoyeong0521@naver.com>
2024-04-26 23:19:57 +0900

    LEVEL1-(1)(2)(3)(4) commit

--- src/App.java
+++ src/App.java
@@ -39,8 +39,6 @@ public class App {
             String input=sc.next();
             if(input.equals("exit")){
                 break;
-            } else {
-                continue;
             }
         }
 
